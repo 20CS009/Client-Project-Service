24e65acb657f2f3fa1f9397933c1b414ef97cf92
Umesha777 <umesha@seabed2crest.com>
2025-10-08 18:32:56 +0530

    first commit

---
+++ src/main/java/com/example/client/project/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.client.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class UserController {
+
+
+
+
+
+
+}
